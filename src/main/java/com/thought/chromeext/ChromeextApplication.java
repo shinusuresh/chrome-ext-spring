6464e3176211460fb487631e7155f7fbebc68420
Shinu Suresh <shinu.suresh@ynap.com>
2020-09-12 20:59:51 +0100

    Initial commit

---
+++ src/main/java/com/thought/chromeext/ChromeextApplication.java
@@ -0,0 +1,13 @@
+package com.thought.chromeext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChromeextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChromeextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thought/chromeext/ChromeextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thought.chromeext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChromeextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
